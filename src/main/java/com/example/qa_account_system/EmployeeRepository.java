72f2c8b84c3d711da59c4977c8f9461389933f29
DarylMConway <Daryl.conway@academytrainee.com>
2018-07-02 15:05:11 +0100

    basics of both springBoot and reactJS

---
+++ src/main/java/com/example/qa_account_system/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.example.qa_account_system;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {}
---
+++ src/main/java/com/example/qa_account_system/QaAccountSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.qa_account_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaAccountSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaAccountSystemApplication.class, args);
+	}
+}
